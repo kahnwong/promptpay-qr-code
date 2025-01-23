23eafacc427308610e41a99ad920f22b8aa21ceb
Karn Wong <karn@karnwong.me>
2025-01-23 21:06:47 +0700

    feat: add inputs

---
+++ src/components/PromptpayInput.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-input class="q-pa-md" dense :label="label" v-model="model"></q-input>
+</template>
+
+<script setup lang="ts">
+export interface PromptpayInputProps {
+  label: string
+  value: string | number | null
+}
+
+withDefaults(defineProps<PromptpayInputProps>(), {})
+
+const model = defineModel({ type: String })
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,29 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <qrcode-vue
-      :value="value"
-      :level="level"
-      :render-as="renderAs"
-      :gradient="gradient"
-      :gradient-type="gradientType"
-      :gradient-start-color="gradientStartColor"
-      :gradient-end-color="gradientEndColor"
-      :size="500"
-    />
+  <q-page class="column items-center">
+    <div style="margin-top: 150px; margin-bottom: 30px">
+      <qrcode-vue
+        :value="value"
+        :level="level"
+        :render-as="renderAs"
+        :gradient="gradient"
+        :gradient-type="gradientType"
+        :gradient-start-color="gradientStartColor"
+        :gradient-end-color="gradientEndColor"
+        :size="500"
+      />
+    </div>
+    <div style="width: 250px">
+      <PromptpayInput
+        v-bind="promptpayID"
+        :label="promptpayID.label"
+        :v-model="promptpayID.value"
+      />
+      <PromptpayInput
+        v-bind="promptpayAmount"
+        :label="promptpayAmount.label"
+        :v-model="promptpayAmount.value"
+      />
+    </div>
   </q-page>
 </template>
 
@@ -18,6 +32,7 @@ import { ref } from 'vue'
 import QrcodeVue from 'qrcode.vue'
 import type { Level, RenderAs, GradientType } from 'qrcode.vue'
 
+// qr code config
 const value = ref('foobarbaz')
 const level = ref<Level>('M')
 const renderAs = ref<RenderAs>('svg')
@@ -26,4 +41,16 @@ const gradient = ref(true)
 const gradientType = ref<GradientType>('linear')
 const gradientStartColor = ref('#000000')
 const gradientEndColor = ref('#38bdf8')
+
+// number input
+import PromptpayInput, { type PromptpayInputProps } from 'components/PromptpayInput.vue'
+
+const promptpayID = ref<PromptpayInputProps>({
+  label: 'PromptPay ID (Personal Only)',
+  value: '0000000000000',
+})
+const promptpayAmount = ref<PromptpayInputProps>({
+  label: 'Amount (THB)',
+  value: null,
+})
 </script>
